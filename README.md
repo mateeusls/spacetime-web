f6559936214d6bf4e2215bafc2967948d284302b
mateeusls <magolopes20@gmail.com>
2023-05-21 08:18:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+<p align="center">
+	<a href="#-Technologies">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-Project">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-Layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<br>
+
+
+<p alignt="center">
+	<img src=".github/preview.jpg">
+</p>
+
+## 🛠 Technologies
+
+This project was development with the technologies:
+
+- ReactJS
+- NextJS(13.4)
+- TailwindCSS
+- Lucide React
+
+### 💻 Project
+
+Spacetime is a project creator on the NLW for list your memories.
+
+## 🔖 Layout
+
+You can visibility layout of project in [site](https://www.figma.com/file/QtFHulKrnXrgoShAaUjw0r/C%C3%A1psula-do-tempo-%E2%80%A2-Trilha-Ignite-(Community)?type=design&node-id=205-85&t=06uznzZf320nULwK-0)
---
+++ src/app/layout.tsx
@@ -0,0 +1,57 @@
+import { Copyright } from '@/components/Copyright'
+import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
+import { SignIn } from '@/components/SignIn'
+import {
+  Bai_Jamjuree as BaiJamJuree,
+  Roboto_Flex as Roboto,
+} from 'next/font/google'
+import { cookies } from 'next/headers'
+import { ReactNode } from 'react'
+import './globals.css'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+
+const baijamjuree = BaiJamJuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-baijamjuree',
+})
+
+export const metadata = {
+  title: 'NLW SpaceTime',
+  description:
+    'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e Typescript',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baijamjuree.variable} bg-gray-900 font-sans text-gray-100 `}
+        suppressHydrationWarning={true}
+      >
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 opacity-50 blur-full" />
+
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
+            {children}
+          </div>
+        </main>
+      </body>
+    </html>
+  )
+}
